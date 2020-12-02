d681cf2ceb6a0e9075efe94df2f57ef289beb157
Kvalium <julien.monchany@gmail.com>
2020-12-02 22:06:43 +0100

    add day 1 solution

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Avent of Code 2020
 
 My solutions...
+
+Each exercise can be executing by running `node index.mjs`
---
+++ day1/index.mjs
@@ -0,0 +1,28 @@
+import { numbers } from "./numbers.mjs";
+
+const oneTermTo2020 = () => {
+  for (const n of numbers) {
+    const rest = 2020 - n;
+    const isRestInNumbers = numbers.includes(rest);
+    if (isRestInNumbers) {
+      return n * rest;
+    }
+  }
+};
+
+const twoTermsTo2020 = () => {
+  for (const n of numbers) {
+    const rest = 2020 - n;
+    const restCandidates = numbers.filter((n) => n < rest);
+    for (const restCandidateA of restCandidates) {
+      for (const restCandidateB of restCandidates) {
+        if (restCandidateA + restCandidateB === rest) {
+          return n * restCandidateA * restCandidateB;
+        }
+      }
+    }
+  }
+};
+
+console.log(`First exercise: ${oneTermTo2020()}`);
+console.log(`Second exercise: ${twoTermsTo2020()}`);
