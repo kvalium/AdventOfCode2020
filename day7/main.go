f25cc7be78e36ca689a7f31f96cdee4da912af1f
Julien Monchany <julien.monchany@gmail.com>
2020-12-09 12:47:08 +0100

    day7 first exercise

--- day7/main.go
+++ day7/main.go
@@ -21,12 +21,28 @@ type bag struct {
 	children []bagContent
 }
 
-var bags map[string]bag
+var bags = make(map[string]*bag)
+var parentBagColors []string
 
 var bagSplitRuleRegex = regexp.MustCompile(`([0-9])\s([a-z ]+)\sbag`)
 
 func main() {
 	getBags("./rules")
+	getParentBagColors(bags["shiny gold"])
+	fmt.Println("First exercise:", len(parentBagColors))
+}
+
+func getParentBagColors(bag *bag) {
+	if len(bag.parents) == 0 {
+		return
+	}
+	for _, parent := range bag.parents {
+		if !inSlice(parentBagColors, parent.color) {
+			parentBagColors = append(parentBagColors, parent.color)
+		}
+		getParentBagColors(bags[parent.color])
+	}
+	return
 }
 
 func getBags(inputFilePath string) {
@@ -49,7 +65,13 @@ func getBags(inputFilePath string) {
 		}
 
 		color, children := getBagRule(bagRuleLine)
-		fmt.Println(color, children)
+
+		if !isBagExists(color) {
+			addNewBag(color)
+		}
+		for _, child := range children {
+			addChildToBag(color, child)
+		}
 	}
 	return
 }
@@ -64,7 +86,41 @@ func getBagRule(bagRuleLine string) (bagColor string, children []bagContent) {
 		ct, _ := strconv.Atoi(string(bc[1]))
 		color := string(bc[2])
 		children = append(children, bagContent{color: color, count: ct})
+		if !isBagExists(color) {
+			addNewBag(color)
+		}
+		addParentToBag(color, bagContent{color: bagColor, count: ct})
 	}
 
 	return
+}
+
+func isBagExists(color string) bool {
+	_, exists := bags[color]
+	return exists
+}
+
+func addNewBag(color string) {
+	bags[color] = &bag{parents: nil, children: nil}
+}
+
+func addParentToBag(color string, parent bagContent) {
+	b := bags[color]
+	b.parents = append(bags[color].parents, parent)
+	bags[color] = b
+}
+
+func addChildToBag(color string, child bagContent) {
+	b := bags[color]
+	b.children = append(bags[color].children, child)
+	bags[color] = b
+}
+
+func inSlice(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
 }
