49850c20ddbe7dc7c9d4081765eb795730670da4
Kvalium <julien.monchany@gmail.com>
2020-12-13 14:44:21 +0100

    day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -2,56 +2,38 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
 func main() {
-	d := getDiffBetweenAdaptersT()
-	fmt.Println("First exercice:", d[1]*d[3])
-	arr := getAllPossibleArrangements()
-	fmt.Println("Second exercice:", arr)
+	diffs, sequences := getDiffBetweenAdapters()
+	fmt.Println("First exercice:", diffs[1]*diffs[3])
+	a := math.Pow(7, sequences[4])
+	b := math.Pow(4, sequences[3])
+	c := math.Pow(2, sequences[2])
+	fmt.Println("Second exercice:", int(a*b*c))
 }
 
-func getDiffBetweenAdaptersT() map[int]int {
+func getDiffBetweenAdapters() (map[int]int, map[int]float64) {
 	var differences = map[int]int{1: 1, 2: 1, 3: 1}
-	sort.Ints(adaptersT)
-	for i, a := range adaptersT {
-		if i+1 == len(adaptersT) {
+	var sequencesOfOne = map[int]float64{
+		1: 0, 2: 1, 3: 0, 4: 0,
+	}
+	sort.Ints(adapters)
+	var diffSeq = 0
+	for i, a := range adapters {
+		if i+1 == len(adapters) {
 			continue
 		}
-		diff := adaptersT[i+1] - a
+		diff := adapters[i+1] - a
 		differences[diff] = differences[diff] + 1
-	}
-	return differences
-}
-
-func getAllPossibleArrangements() (nbArr int) {
-	sort.Ints(adaptersT)
-	return walkAdaptersT(adaptersT, 0, 1)
-}
-
-func walkAdaptersT(adaptersT []int, i, nb int) int {
-	curr := adaptersT[i]
-	if inSlice(adaptersT, curr+1) {
-		nb++
-		nb += walkAdaptersT(adaptersT, i+1, nb)
-	}
-	if inSlice(adaptersT, curr+2) {
-		nb++
-		nb += walkAdaptersT(adaptersT, i+2, nb)
-	}
-	if inSlice(adaptersT, curr+3) {
-		nb++
-		nb += walkAdaptersT(adaptersT, i+3, nb)
-	}
-	return nb
-}
-
-func inSlice(slice []int, val int) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
+		if diff == 3 {
+			sequencesOfOne[diffSeq] = sequencesOfOne[diffSeq] + 1
+			diffSeq = 0
+			continue
 		}
+		diffSeq++
 	}
-	return false
+	return differences, sequencesOfOne
 }
